10a22586a9618531a59ce852d8acc9fa71311121
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-09-23 19:44:27 +0530

    new added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 
 const Home = () => (
-  <div>
+  <div className='home-div'>
     <h1>Welcome to My To-Do App</h1>
     <Link href="/todos">Go to To-Do List</Link>
   </div>
