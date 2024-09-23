6f92e94c40bbc57bb5a22f9b55f6674a8f4438d7
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-09-23 20:12:54 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 // pages/index.tsx
 import Link from 'next/link';
+import '../app/globals.css';
 
 const Home = () => (
-  <div className='home-div'>
-    <h1>Welcome to My To-Do App</h1>
+  <div className='home-div h-[100vh] flex flex-col mx-auto text-center p-8'>
+    <h1 className='text-4xl font-bold'>Welcome to My To-Do App</h1>
+  <div>
+  <button className='border border-black p-4 mt-5 font-bold text-white text-[20px] rounded-md shadow-xl hover:bg-green-600 transition-all duration-500'>
     <Link href="/todos">Go to To-Do List</Link>
-  </div>
+  </button></div> 
+ </div>
 );
 
 export default Home;
